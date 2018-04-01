8ba1aa6899e136c98b204ff0312dfd0cab5af2cc
dindaputriramadhani <37430809+dindaputriramadhani@users.noreply.github.com>
2018-04-01 21:24:01 +0700

    Add files via upload

--- IDNstemmer.php
+++ IDNstemmer.php
@@ -1,201 +1,200 @@
 <?php
-	
-	/*=======================================================================*
-	 * IDNStemmer is a Stemmer Algorithm for Indonesian.		             *
-	/*************************G-P-L*******************************************
-	 *                                                                       *
-	 *  This program is free software: you can redistribute it and/or modify *
-	 *  it under the terms of the GNU General Public License as published by *
-	 *  the Free Software Foundation, either version 3 of the License, or    *
-	 *  (at your option) any later version.                                  *
-	 *                                                                       *  
-	 *  This program is distributed in the hope that it will be useful,      *
-	 *  but WITHOUT ANY WARRANTY; without even the implied warranty of       *
-	 *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
-	 *  GNU General Public License for more details.                         *
-	 *                                                                       *
-	 *  You should have received a copy of the GNU General Public License    *
-	 *  along with this program.  If not, see <http://www.gnu.org/licenses/>.*
-	 *                                                                       *
-	 *************************************************************************
-	 * Copyright 2009 Alfan Farizki Wicaksono [ITB]                          *
-	 *                Bofandra Muhammad [ITB]                                *
-	 *************************************************************************
-	 *************************************************************************/
-	
-	class IDNStemmer
+
+/*=======================================================================*
+ * IDNStemmer is a Stemmer Algorithm for Indonesian.		             *
+/*************************G-P-L*******************************************
+ *                                                                       *
+ *  This program is free software: you can redistribute it and/or modify *
+ *  it under the terms of the GNU General Public License as published by *
+ *  the Free Software Foundation, either version 3 of the License, or    *
+ *  (at your option) any later version.                                  *
+ *                                                                       *  
+ *  This program is distributed in the hope that it will be useful,      *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ *  GNU General Public License for more details.                         *
+ *                                                                       *
+ *  You should have received a copy of the GNU General Public License    *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+ *                                                                       *
+ *************************************************************************
+ * Copyright 2009 Alfan Farizki Wicaksono [ITB]                          *
+ *                Bofandra Muhammad [ITB]                                *
+ *************************************************************************
+ *************************************************************************/
+
+class IDNStemmer
+{
+	function IDNStemmer()
 	{
-		function IDNStemmer()
+	}
+	
+	function del_prefix($masukan)
+	{
+		// awalan : ber, bel, be, ke, se, te, ter, me, mem, men, meng, menge, pe, pem, pen, peng, penge, pe, pel, per, memper; Sumber : http://tatabahasabm.tripod.com/tata/awalan.htm
+		$masukan=strtolower($masukan);
+		$AW1=substr($masukan,0,6);
+		if($AW1=="memper")
 		{
+			$keluaran = substr($masukan,6,strlen($masukan));
 		}
-		
-		function del_prefix($masukan)
+		else
 		{
-			// awalan : ber, bel, be, ke, se, te, ter, me, mem, men, meng, menge, pe, pem, pen, peng, penge, pe, pel, per, memper; Sumber : http://tatabahasabm.tripod.com/tata/awalan.htm
-			$masukan=strtolower($masukan);
-			$AW1=substr($masukan,0,6);
-			if($AW1=="memper")
+			$AW1=substr($masukan,0,3);
+			if($AW1=="ber")
 			{
-				$keluaran = substr($masukan,6,strlen($masukan));
+				$keluaran = substr($masukan,3,strlen($masukan));
 			}
 			else
 			{
-				$AW1=substr($masukan,0,3);
-				if($AW1=="ber")
+				$AW1=substr($masukan,0,4);
+				if($AW1=="bela")
 				{
 					$keluaran = substr($masukan,3,strlen($masukan));
 				}
 				else
 				{
-					$AW1=substr($masukan,0,4);
-					if($AW1=="bela")
+					$AW1=substr($masukan,0,2);
+					if($AW1=="di" )
 					{
-						$keluaran = substr($masukan,3,strlen($masukan));
+						$keluaran = substr($masukan,2,strlen($masukan));
 					}
 					else
 					{
 						$AW1=substr($masukan,0,2);
-						if($AW1=="di" )
+						if($AW1=="ke")
 						{
 							$keluaran = substr($masukan,2,strlen($masukan));
 						}
 						else
 						{
 							$AW1=substr($masukan,0,2);
-							if($AW1=="ke")
+							if($AW1=="ku")
 							{
 								$keluaran = substr($masukan,2,strlen($masukan));
 							}
 							else
 							{
-								$AW1=substr($masukan,0,2);
-								if($AW1=="ku")
+								$AW1=substr($masukan,0,3);
+								if($AW1=="kau")
 								{
-									$keluaran = substr($masukan,2,strlen($masukan));
+									$keluaran = substr($masukan,3,strlen($masukan));
 								}
 								else
 								{
-									$AW1=substr($masukan,0,3);
-									if($AW1=="kau")
+									$AW1=substr($masukan,0,2);
+									if($AW1=="me")
 									{
-										$keluaran = substr($masukan,3,strlen($masukan));
-									}
-									else
-									{
-										$AW1=substr($masukan,0,2);
-										if($AW1=="me")
+										$AW1=substr($masukan,0,4);
+										if($AW1=="memb")
+										{
+											$keluaran = substr($masukan,3,strlen($masukan));
+										}
+										else
 										{
 											$AW1=substr($masukan,0,4);
-											if($AW1=="memb")
+											if($AW1=="mend" || $AW1=="menf" || $AW1=="menj")
 											{
 												$keluaran = substr($masukan,3,strlen($masukan));
 											}
 											else
 											{
 												$AW1=substr($masukan,0,4);
-												if($AW1=="mend" || $AW1=="menf" || $AW1=="menj")
+												if($AW1=="meny")
 												{
-													$keluaran = substr($masukan,3,strlen($masukan));
+													$keluaran = "s".substr($masukan,4,strlen($masukan));
 												}
 												else
 												{
 													$AW1=substr($masukan,0,4);
-													if($AW1=="meny")
+													if($AW1=="meng")
 													{
-														$keluaran = "s".substr($masukan,4,strlen($masukan));
+														if(substr($masukan,4,1)=="a" || substr($masukan,4,1)=="e" || substr($masukan,4,1)=="g" || substr($masukan,4,1)=="h" || substr($masukan,4,1)=="i" || substr($masukan,4,1)=="o" || substr($masukan,4,1)=="u")
+														{
+															$keluaran = substr($masukan,4,strlen($masukan));
+														}
+														else
+														{
+															$keluaran = $masukan;
+														}
 													}
 													else
 													{
-														$AW1=substr($masukan,0,4);
-														if($AW1=="meng")
+														$AW1=substr($masukan,0,3);
+														if($AW1=="men")
 														{
-															if(substr($masukan,4,1)=="a" || substr($masukan,4,1)=="e" || substr($masukan,4,1)=="g" || substr($masukan,4,1)=="h" || substr($masukan,4,1)=="i" || substr($masukan,4,1)=="o" || substr($masukan,4,1)=="u")
+															$keluaran = "t".substr($masukan,3,strlen($masukan));
+														}
+														else
+														{
+															if(substr($masukan,2,1)=="l" || substr($masukan,2,1)=="m" || substr($masukan,2,1)=="n" || substr($masukan,2,1)=="r")
 															{
-																$keluaran = substr($masukan,4,strlen($masukan));
+																$keluaran = substr($masukan,2,strlen($masukan));
 															}
 															else
 															{
 																$keluaran = $masukan;
 															}
 														}
-														else
-														{
-															$AW1=substr($masukan,0,3);
-															if($AW1=="men")
-															{
-																$keluaran = "t".substr($masukan,3,strlen($masukan));
-															}
-															else
-															{
-																if(substr($masukan,2,1)=="l" || substr($masukan,2,1)=="m" || substr($masukan,2,1)=="n" || substr($masukan,2,1)=="r")
-																{
-																	$keluaran = substr($masukan,2,strlen($masukan));
-																}
-																else
-																{
-																	$keluaran = $masukan;
-																}
-															}
-														}
 													}
 												}
 											}
 										}
+									}
+									else
+									{
+										$AW1=substr($masukan,0,2);
+										if($AW1=="pe")
+										{
+											$keluaran = substr($masukan,2,strlen($masukan));
+										}
 										else
 										{
 											$AW1=substr($masukan,0,2);
-											if($AW1=="pe")
+											if($AW1=="se")
 											{
 												$keluaran = substr($masukan,2,strlen($masukan));
 											}
 											else
 											{
-												$AW1=substr($masukan,0,2);
-												if($AW1=="se")
+												$AW1=substr($masukan,0,3);
+												if($AW1=="ter")
 												{
-													$keluaran = substr($masukan,2,strlen($masukan));
+													$keluaran = substr($masukan,3,strlen($masukan));
 												}
 												else
 												{
+												/*
 													$AW1=substr($masukan,0,3);
-													if($AW1=="ter")
+													if($AW1=="eka")
 													{
 														$keluaran = substr($masukan,3,strlen($masukan));
 													}
 													else
 													{
-													/*
-														$AW1=substr($masukan,0,3);
-														if($AW1=="eka")
+														$AW1=substr($masukan,0,6);
+														if($AW1=="ekstra")
 														{
-															$keluaran = substr($masukan,3,strlen($masukan));
+															$keluaran = substr($masukan,6,strlen($masukan));
 														}
 														else
 														{
-															$AW1=substr($masukan,0,6);
-															if($AW1=="ekstra")
+															$AW1=substr($masukan,0,3);
+															if($AW1=="eks")
 															{
-																$keluaran = substr($masukan,6,strlen($masukan));
+																$keluaran = substr($masukan,3,strlen($masukan));
 															}
 															else
 															{
-																$AW1=substr($masukan,0,3);
-																if($AW1=="eks")
+																$AW1=substr($masukan,0,5);
+																if($AW1=="intra")
 																{
-																	$keluaran = substr($masukan,3,strlen($masukan));
-																}
-																else
-																{
-																	$AW1=substr($masukan,0,5);
-																	if($AW1=="intra")
-																	{
-																		$keluaran = substr($masukan,5,strlen($masukan));
-																	}
+																	$keluaran = substr($masukan,5,strlen($masukan));
 																}
 															}
-														}*/
-														$keluaran = $masukan;
-													}
+														}
+													}*/
+													$keluaran = $masukan;
 												}
 											}
 										}
@@ -206,83 +205,83 @@
 					}
 				}
 			}
-			return $keluaran;
 		}
-		
-		function del_suffix($masukan)
+		return $keluaran;
+	}
+	
+	function del_suffix($masukan)
+	{
+		$masukan=strtolower($masukan);
+		$AK = substr($masukan,(strlen($masukan)-3),3);
+		if($AK == "nda")
 		{
-			$masukan=strtolower($masukan);
+			$keluaran = substr($masukan,0,(strlen($masukan)-3));
+		}
+		else
+		{
 			$AK = substr($masukan,(strlen($masukan)-3),3);
-			if($AK == "nda")
+			if($AK == "nya")
 			{
 				$keluaran = substr($masukan,0,(strlen($masukan)-3));
 			}
 			else
 			{
 				$AK = substr($masukan,(strlen($masukan)-3),3);
-				if($AK == "nya")
+				if($AK == "kan" && strlen($masukan)>5)
 				{
 					$keluaran = substr($masukan,0,(strlen($masukan)-3));
 				}
 				else
 				{
-					$AK = substr($masukan,(strlen($masukan)-3),3);
-					if($AK == "kan" && strlen($masukan)>5)
+					$AK = substr($masukan,(strlen($masukan)-2),2);
+					if($AK == "an" && strlen($masukan)>5)
 					{
-						$keluaran = substr($masukan,0,(strlen($masukan)-3));
+						$keluaran = substr($masukan,0,(strlen($masukan)-2));
 					}
 					else
 					{
 						$AK = substr($masukan,(strlen($masukan)-2),2);
-						if($AK == "an" && strlen($masukan)>5)
+						if($AK == "ku" && strlen($masukan)>6)
 						{
 							$keluaran = substr($masukan,0,(strlen($masukan)-2));
 						}
 						else
 						{
 							$AK = substr($masukan,(strlen($masukan)-2),2);
-							if($AK == "ku" && strlen($masukan)>6)
+							if($AK == "mu" && strlen($masukan)>6)
 							{
 								$keluaran = substr($masukan,0,(strlen($masukan)-2));
 							}
 							else
 							{
-								$AK = substr($masukan,(strlen($masukan)-2),2);
-								if($AK == "mu" && strlen($masukan)>6)
+								$AK = substr($masukan,(strlen($masukan)-1),1);
+								if($AK == "i" && strlen($masukan)>6)
 								{
-									$keluaran = substr($masukan,0,(strlen($masukan)-2));
+									$keluaran = substr($masukan,0,(strlen($masukan)-1));
 								}
 								else
 								{
-									$AK = substr($masukan,(strlen($masukan)-1),1);
-									if($AK == "i" && strlen($masukan)>6)
-									{
-										$keluaran = substr($masukan,0,(strlen($masukan)-1));
-									}
-									else
-									{
-										$keluaran = $masukan;
-									}
+									$keluaran = $masukan;
 								}
 							}
 						}
 					}
 				}
 			}
-			return $keluaran;
 		}
-		
-		function doStemming($word)
-		{
-			$p1 = $this->del_prefix($word);
-			$p2 = $this->del_prefix($p1);
-			$p3 = $this->del_prefix($p1);
-			$s1 = $this->del_suffix($p3);
-			$s2 = $this->del_suffix($s1);
-			
-			return $s2;
-		}
+		return $keluaran;
 	}
 	
-	?>
+	function doStemming($word)
+	{
+		$p1 = $this->del_prefix($word);
+		$p2 = $this->del_prefix($p1);
+		$p3 = $this->del_prefix($p1);
+		$s1 = $this->del_suffix($p3);
+		$s2 = $this->del_suffix($s1);
+		
+		return $s2;
+	}
+}
 
+?>
